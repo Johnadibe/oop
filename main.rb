cd6efe429981c7fa422ace1180cf89c1f18147dd
Donmark2k <chukwuemekavp@gmail.com>
2023-04-17 15:07:56 +0100

    Rewrite the main method

---
+++ main.rb
@@ -0,0 +1,19 @@
+require_relative 'refactor'
+
+def main
+  puts "Welcome to School library App!\n"
+  refactor = Refactor.new
+  loop do
+    refactor.display_selection
+    option = gets.chomp.to_i
+    case option
+    when 1..6
+      refactor.list_holder(option)
+    else
+      puts 'Thank you for using this app!'
+      break
+    end
+  end
+end
+
+main
