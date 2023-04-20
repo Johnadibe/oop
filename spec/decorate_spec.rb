0232f9324997accfaff0e7d8074e4b4bbaf38eb5
Donmark2k <chukwuemekavp@gmail.com>
2023-04-20 16:13:53 +0100

    Correct linters error

--- spec/decorate_spec.rb
+++ spec/decorate_spec.rb
@@ -2,7 +2,7 @@ require './decorate'
 
 describe CapitalizeDecorator do
   context 'Capitalize  name' do
-    capitalizeDecorator = CapitalizeDecorator.new ('chiamanda')
+    capitalizeDecorator = CapitalizeDecorator.new('chiamanda')
 
     it 'Capitalize the name' do
       expect(capitalizeDecorator.correct_name).to be 'Chiamanda'
