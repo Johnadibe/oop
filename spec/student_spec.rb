7a5f2be7bdc040cf9c32f71e88f56f1d21f7f8d1
Donmark2k <chukwuemekavp@gmail.com>
2023-04-20 13:00:37 +0100

    Create a teacher testing file

---
+++ spec/student_spec.rb
@@ -0,0 +1,22 @@
+require './student'
+
+describe Student do
+  context 'create a student' do
+    student = Student.new(48,'Chukwuemeka','ENG121',  parent_permission: true)
+
+    it 'show name' do
+      expect(student.name).to eq 'Chukwuemeka'
+    end
+
+    it 'show classroom' do
+        expect(student.classroom).to eq 'ENG121'
+      end
+
+    it 'and age' do
+      expect(student.age).to eq 48
+    end
+
+   
+    
+  end
+end
